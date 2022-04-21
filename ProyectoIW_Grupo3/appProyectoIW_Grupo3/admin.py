93aa274c8199758e2be494ce84b2621672bdf795
julenmarquez <julen.marquez@opendeusto.es>
2022-04-21 17:55:02 +0200

    Paso 6

--- ProyectoIW_Grupo3/appProyectoIW_Grupo3/admin.py
+++ ProyectoIW_Grupo3/appProyectoIW_Grupo3/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Equipo, Ticket, Empleado
 
-# Register your models here.
+admin.site.register(Equipo)
+admin.site.register(Ticket)
+admin.site.register(Empleado)
