7390064b5fa8da49bffbfd41486779d383948832
julenmarquez <julen.marquez@opendeusto.es>
2022-04-21 17:50:41 +0200

    Paso 5

--- ProyectoIW_Grupo3/appProyectoIW_Grupo3/models.py
+++ ProyectoIW_Grupo3/appProyectoIW_Grupo3/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Equipo(models.Model):
+    numeroSerie= models.IntegerField()
+    modelo = models.CharField(max_length=150)
+    marca = models.CharField(max_length=30)
+    tipoEquipo = models.CharField(max_length=30)
+    fechaAdquisicion = models.DateField
+    fechaPuestaMarcha = models.DateField
+    proveedorNombre = models.CharField(max_length=30)
+    proveedorTelefono = models.IntegerField()
+    planta = models.CharField(max_length=100)
+
+
+class Empleado(models.Model):
+    DNI = models.CharField(max_length=9)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    telfono = models.IntegerField()
+
+
+class Ticket(models.Model):
+    numeroSerieEquipo= models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    DNIempleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+
+    #numeroReferencia = models.IntegerField()
+    titulo = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=300)
+    fechaApertura = models.DateField
+    fechaResolucion = models.DateField
+    nivelUrgencia = models.CharField(max_length=5)
+    tipoTicket = models.CharField(max_length=20)
+    estado = models.CharField(max_length=30)
+    comentarios = models.CharField(max_length=500)
