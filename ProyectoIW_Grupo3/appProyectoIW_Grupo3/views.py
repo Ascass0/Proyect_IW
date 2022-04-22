85bfc02f13d6ef0366a2ff379a186b6067fa9f79
Industria 4.0 <ibai.muguruza@opendeusto.es>
2022-04-22 17:06:53 +0200

    Paso 8 - Vista principal y vista listado equipos

--- ProyectoIW_Grupo3/appProyectoIW_Grupo3/views.py
+++ ProyectoIW_Grupo3/appProyectoIW_Grupo3/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import Equipo, Ticket, Empleado
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Listado de SETAS")
+    return render(request, 'index.html')
+
+#devuelve los equipos
+def listado_equipos(request):
+	equipos = get_list_or_404(Equipo.objects.order_by('numeroSerie'))
+	context = {'equipo': equipos }
+	return render(request, 'listadoEquipos.html', context)
