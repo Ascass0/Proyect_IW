787545610b8ed515c582824f958f138f2e7da94e
ibaimuguruza <ibai.muguruza@opendeusto.es>
2022-05-24 18:41:11 +0200

    zoom 2 niveles pocho

---
+++ ProyectoIW_Grupo3/static/javaScript/zoom.js
@@ -0,0 +1,7 @@
+
+document.getElementById("zoomIn").addEventListener("click", function() {
+    document.getElementsByTagName("table")[0].style.fontSize = '40px';
+});
+document.getElementById("zoomOut").addEventListener("click", function() {
+    document.getElementsByTagName("table")[0].style.fontSize = '3px';
+});
