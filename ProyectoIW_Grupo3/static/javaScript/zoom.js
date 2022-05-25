d7666b18bf50beb3ba9999e0a7a6120386c6c5af
ibaimuguruza <ibai.muguruza@opendeusto.es>
2022-05-25 18:53:57 +0200

    zoom progresivo(falta footer)

--- ProyectoIW_Grupo3/static/javaScript/zoom.js
+++ ProyectoIW_Grupo3/static/javaScript/zoom.js
@@ -1,7 +1,33 @@
+var contMas = 0;
+var contMenos = 0;
 
-document.getElementById("zoomIn").addEventListener("click", function() {
-    document.getElementsByTagName("table")[0].style.fontSize = '40px';
+var a = document.getElementsByTagName("table")[0];
+var pixelesActuales = window.getComputedStyle(a, null).getPropertyValue('font-size');
+
+
+function contadorPlus() {
+    contMenos = 0;
+
+    pixelesActuales = window.getComputedStyle(a, null).getPropertyValue('font-size');
+
+
+    contMas = contMas + 3;
+    var tamaño = (parseInt(pixelesActuales.replace(/px/, "")) + contMas) + "px";
+    document.getElementsByTagName("table")[0].style.fontSize = tamaño;
+}
+
+function contadorMinus() {
+    contMas = 0;
+
+    pixelesActuales = window.getComputedStyle(a, null).getPropertyValue('font-size');
+
+    contMenos = contMenos + 3;
+    var tamaño = (parseInt(pixelesActuales.replace(/px/, "")) - contMenos) + "px";
+    document.getElementsByTagName("table")[0].style.fontSize = tamaño;
+}
+
+document.getElementById("restablecer").addEventListener("click", function() {
+    document.getElementsByTagName("table")[0].style.fontSize = '16px';
 });
-document.getElementById("zoomOut").addEventListener("click", function() {
-    document.getElementsByTagName("table")[0].style.fontSize = '3px';
-});
+document.getElementById("zoomIn").addEventListener("click", contadorPlus);
+document.getElementById("zoomOut").addEventListener("click", contadorMinus);
